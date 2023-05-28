8c7d2eb6764032a4e6d8dfb1015b81333e289173
Telpenarmo <telpenarmo@protonmail.com>
2023-05-28 22:41:12 +0200

    Implement basic orders lifecycle logic

--- src/DDDDelivery.Domain/Model/Order.fs
+++ src/DDDDelivery.Domain/Model/Order.fs
@@ -12,13 +12,15 @@ module Order =
           worth: float
           discount: float }
 
+    type CancellationReason = { reason: string }
+
     type OrderStatus =
         | Pending
         | Processed
         | Awaiting
         | Shipped
         | Delivered
-        | Cancelled
+        | Cancelled of CancellationReason
 
     [<NoEquality; NoComparison>]
     type Order =
@@ -26,6 +28,59 @@ module Order =
           status: OrderStatus
           customer: Customer.CustomerId
           // TODO: shipment:
-          orders: OrderLine seq
+          orderLines: OrderLine seq
           orderTime: DateTime
-          expectedShipmentTime: DateTime }
+          expectedShipmentTime: DateTime
+          modifiedAt: DateTime }
+
+    let cancellable =
+        function
+        | Delivered
+        | Cancelled _ -> false
+        | _ -> true
+
+    let internal create id customerId orderLines expectedDeliveryDays =
+        { id = id
+          status = Pending
+          customer = customerId
+          orderLines = orderLines
+          orderTime = DateTime.Now
+          expectedShipmentTime =
+            DateTime.Now
+            + TimeSpan.FromDays expectedDeliveryDays
+          modifiedAt = DateTime.Now }
+
+    module Lens =
+        let changeStatus f order =
+            f order.status
+            |> Option.map (fun status ->
+                { order with
+                    modifiedAt = DateTime.Now
+                    status = status })
+
+    module Commands =
+        type Command = Order -> Order option
+
+        let private advanceTo next expected actual =
+            if actual = expected then
+                Some next
+            else
+                None
+
+        let cancel reason : Command =
+            let tryCancel =
+                fun status ->
+                    if cancellable status then
+                        Some(Cancelled { reason = reason })
+                    else
+                        None
+
+            Lens.changeStatus tryCancel
+
+        let accept: Command = Lens.changeStatus (Pending |> advanceTo Processed)
+
+        let prepare: Command = Lens.changeStatus (Processed |> advanceTo Awaiting)
+
+        let ship: Command = Lens.changeStatus (Awaiting |> advanceTo Shipped)
+
+        let deliver: Command = Lens.changeStatus (Shipped |> advanceTo Delivered)
